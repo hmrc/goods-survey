a83bbb138411eb7855d3ba45b9ebe16c5c479a3f
Nic Fellows <nic@carboninternet.net>
2020-01-10 13:54:50 +0000

    Add welsh support

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var cookieSession = require('cookie-session')
 var createError = require('http-errors');
 var express = require('express');
+var i18n = require("i18n");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
 
@@ -14,6 +16,30 @@ dotenv.config();
 
 var app = express();
 
+app.use(cookieParser());
+
+i18n.configure({
+  locales:['en', 'cy'],
+  directory: __dirname + '/locales',
+  cookie: 'lang',
+  queryParameter: 'lang'
+});
+
+app.use(i18n.init);
+
+app.use(function(req, res, next) {
+  if(req.query.lang)
+    res.cookie('lang', req.query.lang, { maxAge: 900000, httpOnly: true });
+
+  next();
+})
+
+hbs.registerHelper('__', function () {
+  return i18n.__.apply(this, arguments);
+});
+hbs.registerHelper('__n', function () {
+  return i18n.__n.apply(this, arguments);
+});
 
 app.use(cookieSession({
   name: 'session',
@@ -28,7 +54,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
---
+++ locales/languages.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+
+df_locales = pd.read_csv('locales.tsv', delimiter='\t')
+
+
+df_locales = df_locales[df_locales.columns[:3]]
+
+df_locales.columns = ['label', 'en', 'cy']
+
+df_locales.set_index('label', inplace=True)
+
+
+with open("Output.json", "w") as f:
+    print(json.dumps(df_locales['en'].to_dict(), indent=2), file=f)
