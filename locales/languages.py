eafd136cf8803d555e0dd2eff3446693abc03d94
Nic Fellows <nic@carboninternet.net>
2020-01-10 15:23:22 +0000

    Tweak clothes

--- locales/languages.py
+++ locales/languages.py
@@ -11,5 +11,8 @@ df_locales.columns = ['label', 'en', 'cy']
 df_locales.set_index('label', inplace=True)
 
 
-with open("Output.json", "w") as f:
+with open("en.json", "w") as f:
     print(json.dumps(df_locales['en'].to_dict(), indent=2), file=f)
+    
+with open("cy.json", "w") as f:
+    print(json.dumps(df_locales['cy'].to_dict(), indent=2), file=f)
